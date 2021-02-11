66b325751d77da93d5fd2af98a6c3118b072ffea
Naitik Mundra <mundranaitik@gmail.com>
2021-02-11 18:32:24 +0530

    Basic setup

---
+++ src/config/settings.py
@@ -0,0 +1,33 @@
+from enum import Enum
+from pathlib import Path
+from pydantic import BaseSettings
+
+env_file_path = Path(__file__).parent
+
+
+class AppMode(Enum):
+    DEV = "development"
+    PROD = "production"
+
+
+class Settings(BaseSettings):
+    fastapi_env: AppMode = AppMode.DEV
+    name: str
+    secret_key: str
+    algorithm: str
+    # Postgres connection settings
+    postgres_db: str
+    postgres_host: str
+    postgres_port: int
+    postgres_user: str
+    postgres_password: str
+
+    class Config:
+        env_file = env_file_path
+
+    @property
+    def is_dev(self) -> bool:
+        return self.fastapi_env == AppMode.DEV
+
+
+settings = Settings()
---
+++ src/main.py
@@ -0,0 +1,35 @@
+"""
+Server Init file.
+"""
+import logging
+from pathlib import Path
+import sys
+
+from fastapi import FastAPI
+
+from api.users.routes import router as user_router, add_piccolo_user_crud
+from config.settings import settings
+from custom_logging import custom_logging
+
+# Start Logger
+logger = logging.getLogger(__name__)
+config_path = Path(__file__).absolute().with_name("logging_config.json")
+
+
+try:
+    app: FastAPI = FastAPI(name=settings.name, title=settings.name)
+    logger = custom_logging.CustomizeLogger.make_logger(config_path)
+    logger.add(f"/var/{settings.name.lower()}.log")
+    logger.info("Initialized app")
+except ModuleNotFoundError:
+    logger.error("FastAPI likely not found")
+    sys.exit(1)
+
+
+try:
+    app.include_router(user_router, tags=["Auth"])
+    logger.info("Added user router")
+    add_piccolo_user_crud(app)
+except Exception as exception:
+    logger.error(f"Could not add user router\n{exception}")
+    sys.exit(1)
