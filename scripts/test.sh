6fa8fed8f0f26496e01421e719658a45fb5a3d30
Naitik Mundra <mundranaitik@outlook.com>
2021-03-13 18:16:21 +0530

    Remove semicolons

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 if [[ -z "${TRAVIS}"]]; then
-  bash scripts/fresh_database_and_tables.sh;
+  bash scripts/fresh_database_and_tables.sh
 fi
-python create_admin_user.py;
-python -m pytest -s;
+python create_admin_user.py
+python -m pytest -s
