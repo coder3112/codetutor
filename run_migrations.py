d712f2f9f2ccb7a9681940e6e49ca12918065455
Naitik Mundra <mundranaitik@outlook.com>
2021-03-13 18:17:31 +0530

    Removed f string in run_migrations.py (no need)

--- run_migrations.py
+++ run_migrations.py
@@ -3,7 +3,7 @@ import os
 commands = [
     "piccolo migrations new user --auto; piccolo migrations forwards user",
     "piccolo migrations new session_auth --auto; piccolo migrations forwards session_auth",
-    f"piccolo migrations new models --auto; piccolo migrations forwards models;",
+    "piccolo migrations new models --auto; piccolo migrations forwards models;",
 ]
 
 for command in commands:
