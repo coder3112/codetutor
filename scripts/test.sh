d84cca5fe4ec520013642fe6d4e5134577bd56ba
Naitik Mundra <naitikm@naitiks-macbook-air.home>
2021-07-02 14:29:43 +0200

    Removed travis from github repo

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [[ !${TRAVIS+x} ]]; then
-  bash scripts/fresh_database_and_tables.sh
-fi
+bash scripts/fresh_database_and_tables.sh
 python create_admin_user.py
 python -m pytest -s
